04d25790e8c98895dad40f86b3d4973a6e8c123a
Mohamed Jameel <jameel.r@expressvpn.com>
2017-02-16 14:47:26 +0800

    force build to fail

--- HelloWorld_spec.rb
+++ HelloWorld_spec.rb
@@ -5,7 +5,7 @@ describe HelloWorld do
     it "The say_hello method should return 'Hello World'" do
       hw = HelloWorld.new
       message = hw.say_hello
-      puts 'Succeed' if expect(message).to eq "Hello, world!"
+      puts 'Succeed' if expect(message).to eq "Hello, sworld!"
     end
   end
 end
